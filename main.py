6e117229e397760043a7b8a3ab1329a43290bdfa
Alexander Behlok <abehl@LLE.ROCHESTER.EDU>
2024-01-09 16:59:06 -0500

    new

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import os
-import ocrmypdf
-import os
+import PyPDF2
 import ocrmypdf
 import requests
 from bs4 import BeautifulSoup
 import os
+from langchain.indexes.vectorstore import VectorstoreIndexCreator
+from langchain.document_loaders.pdf import PyPDFLoader
+from langchain.vectorstores import VectorStore
 
 
 def ocr_pdfs(input_path: str, output_path: str):
@@ -30,3 +31,19 @@ def get_pdfs(url: str):
         filename = os.path.basename(pdf_link)
         with open(filename, 'wb') as file:
             file.write(response.content)
+
+
+def get_text_as_str(pdf_path: str, vectorstore: VectorStore):
+    pdf_loader = PyPDFLoader(pdf_path)
+    vectorstore = VectorstoreIndexCreator.from_loaders(loaders=[pdf_loader])
+
+
+def main():
+    pdf_loader = PyPDFLoader()
+    vectorstore = VectorstoreIndexCreator.from_loaders(loaders=[])
+
+    return
+
+
+if __name__ == '__main__':
+    main()
